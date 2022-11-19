e197994c650ac61671e9a7dba229121f426d7677
immutable-dev <immutable-org@protonmail.com>
2022-11-19 12:09:22 -0500

    Update config to build index files for static routing

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const shouldAnalyzeBundles = process.env.ANALYZE === true;
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: './',
+  trailingSlash: true,
 }
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
