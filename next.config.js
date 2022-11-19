1d2c74176884273d46d5ad544338828935df8999
immutable-dev <immutable-org@protonmail.com>
2022-11-19 08:57:30 -0500

    Local build chunk loading fix

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const shouldAnalyzeBundles = process.env.ANALYZE === true;
 
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
   assetPrefix: './',
-
 }
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
