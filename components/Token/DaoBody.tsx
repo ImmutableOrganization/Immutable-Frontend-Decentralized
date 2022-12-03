d9027e9e0f69c3daaa8066ed24e3fd18b271d826
immutable-developer <immutable-org@protonmail.com>
2022-12-03 11:58:44 -0500

    code quality enforcement

--- components/Token/DaoBody.tsx
+++ components/Token/DaoBody.tsx
@@ -1,62 +1,47 @@
 export const DaoBody: React.FC = () => {
-  return (
-    <>
-      <ul>
-        <h3>INFO:</h3>
-        <li>
-          Immutable Token is used for governance of the Immutable Organization.
-        </li>
-        <li>
-          All fees collected by smart contracts operated by the Immutable
-          Organization will be distributed proportionally to all token holders.
-        </li>
-      </ul>
+	return (
+		<>
+			<ul>
+				<h3>INFO:</h3>
+				<li>Immutable Token is used for governance of the Immutable Organization.</li>
+				<li>All fees collected by smart contracts operated by the Immutable Organization will be distributed proportionally to all token holders.</li>
+			</ul>
 
-      <ul>
-        <h3>TOKEN:</h3>
-        <li>ERC 20 Token.</li>
-        <li>
-          TOTAL SUPPLY: 20,000,000.
-          <ul>
-            <li>49% Liquidity Pool.</li>
-            <li>20% Organization founders.</li>
-            <li>11% Organization treasury.</li>
-            <li>
-              The remaining 20% will be under the control of the Organization
-              until further progress has been made for phase 1.0. It will be decided if this remaining amount
-              will be burned or unlocked.
-            </li>
-          </ul>
-        </li>
+			<ul>
+				<h3>TOKEN:</h3>
+				<li>ERC 20 Token.</li>
+				<li>
+					TOTAL SUPPLY: 20,000,000.
+					<ul>
+						<li>49% Liquidity Pool.</li>
+						<li>20% Organization founders.</li>
+						<li>11% Organization treasury.</li>
+						<li>
+							The remaining 20% will be under the control of the Organization until further progress has been made for phase 1.0. It will be
+							decided if this remaining amount will be burned or unlocked.
+						</li>
+					</ul>
+				</li>
 
-        <li>
-          Track IMT on DEXTools:{' '}
-          <a
-            target="_blank"
-            href="https://www.dextools.io/app/en/arbitrum/pair-explorer/0x36a524072f8f2ec359428df28e7ec169d3a82807"
-          >
-            DEXTools link.
-          </a>
-        </li>
-        <li>
-          Arbitrum:{' '}
-          <a
-            target="_blank"
-            href="https://arbiscan.io/token/0x17482Fae07cF2fF29233C4c4e29B5deF5130B7a1"
-          >
-            0x17482Fae07cF2fF29233C4c4e29B5deF5130B7a1
-          </a>
-        </li>
-        <li>
-          Mainnet:{' '}
-          <a
-            target="_blank"
-            href="https://etherscan.io/token/0xA3847dDbC97C6D3AcD265f0A42B3b885e5f8865e"
-          >
-            0xA3847dDbC97C6D3AcD265f0A42B3b885e5f8865e
-          </a>
-        </li>
-      </ul>
-    </>
-  )
-}
+				<li>
+					Track IMT on DEXTools:{' '}
+					<a target='_blank' href='https://www.dextools.io/app/en/arbitrum/pair-explorer/0x36a524072f8f2ec359428df28e7ec169d3a82807'>
+						DEXTools link.
+					</a>
+				</li>
+				<li>
+					Arbitrum:{' '}
+					<a target='_blank' href='https://arbiscan.io/token/0x17482Fae07cF2fF29233C4c4e29B5deF5130B7a1'>
+						0x17482Fae07cF2fF29233C4c4e29B5deF5130B7a1
+					</a>
+				</li>
+				<li>
+					Mainnet:{' '}
+					<a target='_blank' href='https://etherscan.io/token/0xA3847dDbC97C6D3AcD265f0A42B3b885e5f8865e'>
+						0xA3847dDbC97C6D3AcD265f0A42B3b885e5f8865e
+					</a>
+				</li>
+			</ul>
+		</>
+	);
+};
--- pages/decentralizedchat.tsx
+++ pages/decentralizedchat.tsx
@@ -1,71 +1,76 @@
-import { NextPage } from 'next'
-import { useEffect, useState } from 'react'
-import * as openpgp from 'openpgp'
-import { Frame } from '../components/Frame'
+import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import * as openpgp from 'openpgp';
+import { Frame } from '../components/Frame';
+import { useLocalStorage } from 'usehooks-ts';
 
-let ranOnce = false
+let ranOnce = false;
 
 const DecentralizedChat: NextPage = () => {
-  // onload check if keys exist
-  // if not prompt user to create
-  // they can deny
+	// onload check if keys exist
+	// if not prompt user to create
+	// they can deny
 
-  // display encryption status at top of page
-  // display public key top of page with copy button
+	// display encryption status at top of page
+	// display public key top of page with copy button
 
-  const [encryptionEnabled, setEncryptionEnabled] = useState<boolean>(false);
-  const [publicKey, setPublicKey] = useState<string>('');
-  
+	const [publicKey, setPublicKey] = useLocalStorage<string>('publicKey', '');
+	const [privateKey, setPrivateKey] = useLocalStorage<string>('privateKey', '');
+	const [passphrase, setPassPhrase] = useLocalStorage<string>('passphrase', '');
+	const [encryptionEnabled, setEncryptionEnabled] = useLocalStorage<boolean>('encryptionEnabled', false);
 
+	useEffect(() => {
+		if (!ranOnce) {
+		}
+	}, []);
 
-  useEffect(() => {
-    if (!ranOnce) {
-    }
-  }, [])
+	const encryptionBody = () => {
+		return (
+			<>
+				<h2>Encryption is {encryptionEnabled ? 'enabled' : 'disabled'}</h2>
+				but you can have the keys and still disable encryption right we would need to check if user has the keys ready
+			</>
+		);
+	};
 
-  const encryptionBody = () => {
-    return(
-      <>
+	const genKeyPair = async () => {
+		// reqs name email comment
 
-        <h2>Encryption is {encryptionEnabled ? 'enabled' : 'disabled'}</h2>
+		console.log('generating key pair');
 
-      </>
-    )
-  }
+		const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
+			type: 'ecc', // Type of the key, defaults to ECC
+			curve: 'curve25519', // ECC curve name, defaults to curve25519
+			userIDs: [
+				{
+					name: 'Jon Smith',
+					email: 'jon@example.com',
+					comment: '',
+				},
+			], // you can pass multiple user IDs
+			passphrase: 'super long and hard to guess secret', // protects the private key
+			format: 'armored', // output key format, defaults to 'armored' (other options: 'binary' or 'object')
+		});
 
-  const genKeyPair = async () => {
-    // reqs name email comment
+		// ask if would like to save to device storage
+		// if not it will only last during this browser session
 
-    console.log('generating key pair')
+		console.log(privateKey); // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+		console.log(publicKey); // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+		console.log(revocationCertificate); // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+	};
 
-    const {
-      privateKey,
-      publicKey,
-      revocationCertificate,
-    } = await openpgp.generateKey({
-      type: 'ecc', // Type of the key, defaults to ECC
-      curve: 'curve25519', // ECC curve name, defaults to curve25519
-      userIDs: [{ name: 'Jon Smith', email: 'jon@example.com', comment: '' }], // you can pass multiple user IDs
-      passphrase: 'super long and hard to guess secret', // protects the private key
-      format: 'armored', // output key format, defaults to 'armored' (other options: 'binary' or 'object')
-    })
+	// signing is for authentication, anyone with your public key can decrypt it and verify its you
+	// encrypting is for secure messaging, where you require someones public key to write a message to them.
+	// =in big pools you can encrypt to many people
+	// you would get public key from peers on connection., they send public key etc
 
-    console.log(privateKey) // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
-    console.log(publicKey) // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
-    console.log(revocationCertificate) // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
-  }
-
-  return (
-    <>
-      <Frame headerText={'ENCRYPTED'} body={encryptionBody} />
-      <input
-        type="button"
-        className="button"
-        onClick={() => genKeyPair()}
-        value="genKeyPair"
-      />
-    </>
-  )
-}
+	return (
+		<>
+			<Frame headerText={'ENCRYPTED'} body={encryptionBody} />
+			<input type='button' className='button' onClick={() => genKeyPair()} value='genKeyPair' />
+		</>
+	);
+};
 
 export default DecentralizedChat;
