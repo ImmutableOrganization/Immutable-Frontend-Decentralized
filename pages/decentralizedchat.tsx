880bb60cdab3eaaef0c7281d3f26cab442a950e9
immutable-developer <immutable-org@protonmail.com>
2022-12-03 20:56:02 -0500

    info and app page plus bittorrent peer conn

--- pages/decentralizedchat.tsx
+++ pages/decentralizedchat.tsx
@@ -1,10 +1,9 @@
 import { NextPage } from 'next';
 import { useEffect, useState } from 'react';
-import * as openpgp from 'openpgp';
+// import * as openpgp from 'openpgp';
 import { Frame } from '../components/Frame';
 import { useLocalStorage } from 'usehooks-ts';
-import { BaseRoomConfig, joinRoom } from 'trystero';
-import { TorrentRoomConfig } from 'trystero/torrent';
+import { BaseRoomConfig, joinRoom, Room } from 'trystero';
 let ranOnce = false;
 
 const DecentralizedChat: NextPage = () => {
@@ -20,6 +19,9 @@ const DecentralizedChat: NextPage = () => {
 	const [passphrase, setPassPhrase] = useLocalStorage<string>('passphrase', '');
 	const [encryptionEnabled, setEncryptionEnabled] = useLocalStorage<boolean>('encryptionEnabled', false);
 
+	const [sendMessageState, setSendMessageState] = useState<any>();
+	const [rooms, setRooms] = useState<Room[]>();
+
 	useEffect(() => {
 		if (!ranOnce) {
 			// first just do unencrypted chat right lol.
@@ -29,7 +31,9 @@ const DecentralizedChat: NextPage = () => {
 			const _appId = 'immutable';
 
 			// _roomname - A string to namespace peers and events within a room.
-			const _roomName = 'room22';
+			const _roomName = 'room22213213qsadsad';
+			console.log('hi');
+			const password = 'public';
 
 			// CONFIG CAN TAKE A PASSWORD TO MASK PEERS TO NOT USERS OF THE APP
 			//password, encrypts the SID for peers , session descriptions will be encrypted using AES-CBC
@@ -38,9 +42,28 @@ const DecentralizedChat: NextPage = () => {
 
 			// other options params we can skip
 			const config: BaseRoomConfig = { appId: _appId };
-			const room = joinRoom(config, _roomName);
-			room.leave();
-			joinRoom;
+			// const room = joinRoom(config, _roomName);
+
+			if (rooms) {
+				// should probably chekc if doesnt exist already
+				// setRooms((rooms: any) => [...rooms, room]);
+			}
+
+			// room.leave();
+			// room.getPeers();
+			// room.onPeerJoin((peerId) => console.log(`${peerId} joined`));
+			// room.onPeerLeave((peerId) => console.log(`${peerId} left`));
+
+			// const [sendMessage, getMessage, inprogressMessage] = room.makeAction('message');
+
+			// setSendMessageState(sendMessage);
+
+			// getMessage((_msg, peerId: string, metadata: any) => {
+			// console.log('hi!');
+			// });
+
+			// inprogressMessage((percent, peerId, metadata) => console.log(`${percent * 100}% done receiving  from ${peerId}`));
+			ranOnce = true;
 		}
 	}, []);
 
@@ -58,26 +81,28 @@ const DecentralizedChat: NextPage = () => {
 
 		console.log('generating key pair');
 
-		const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
-			type: 'ecc', // Type of the key, defaults to ECC
-			curve: 'curve25519', // ECC curve name, defaults to curve25519
-			userIDs: [
-				{
-					name: 'Jon Smith',
-					email: 'jon@example.com',
-					comment: '',
-				},
-			], // you can pass multiple user IDs
-			passphrase: 'super long and hard to guess secret', // protects the private key
-			format: 'armored', // output key format, defaults to 'armored' (other options: 'binary' or 'object')
-		});
+		// const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
+		// 	type: 'ecc', // Type of the key, defaults to ECC
+		// 	curve: 'curve25519', // ECC curve name, defaults to curve25519
+		// 	userIDs: [
+		// 		{
+		// 			name: 'Jon Smith',
+		// 			email: 'jon@example.com',
+		// 			comment: '',
+		// 		},
+		// 	], // you can pass multiple user IDs
+		// 	passphrase: 'super long and hard to guess secret', // protects the private key
+		// 	format: 'armored', // output key format, defaults to 'armored' (other options: 'binary' or 'object')
+		// });
 
 		// ask if would like to save to device storage
 		// if not it will only last during this browser session
+		// import { useCopyToClipboard } from 'usehooks-ts'
+		// this hook would be good to use for saving for user to
 
-		console.log(privateKey); // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
-		console.log(publicKey); // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
-		console.log(revocationCertificate); // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+		// console.log(privateKey); // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+		// console.log(publicKey); // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+		// console.log(revocationCertificate); // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
 	};
 
 	// signing is for authentication, anyone with your public key can decrypt it and verify its you
@@ -94,8 +119,20 @@ const DecentralizedChat: NextPage = () => {
 				onClick={() => setEncryptionEnabled(!encryptionEnabled)}
 				value={`${encryptionEnabled ? 'DISABLE' : 'ENABLE'} ENCRYPTION`}
 			/>
+			<input type='button' className='button' onClick={() => sendMessageState()} value='Send Message' />
 
 			<input type='button' className='button' onClick={() => genKeyPair()} value='genKeyPair' />
+			{rooms ? (
+				<>
+					{rooms.map((_room: Room, listIndex: number) => (
+						<>
+							<p>listIndex : {listIndex}</p>
+						</>
+					))}
+				</>
+			) : (
+				<>Please join a room.</>
+			)}
 		</>
 	);
 };
